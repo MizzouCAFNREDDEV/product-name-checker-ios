f9b696a479c2d5e03d61fe2dbebaa4652e513883
Michael Smith <mdsf3f@mail.missouri.edu>
2017-10-01 23:14:45 -0500

    Initial commit

---
+++ Product Name Checker/SimplePDF.swift
@@ -0,0 +1,9 @@
+//
+//  SimplePDF.swift
+//  Product Name Checker
+//
+//  Created by Michael Smith on 10/1/17.
+//  Copyright © 2017 CAFNR EDDEV. All rights reserved.
+//
+
+import Foundation
---
+++ Product Name Checker/ViewPDFController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewPDFController.swift
+//  Product Name Checker
+//
+//  Created by Michael Smith on 10/1/17.
+//  Copyright © 2017 CAFNR EDDEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewPDFController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
